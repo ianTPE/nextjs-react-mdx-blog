81e751e8c9d8093d0fbf1fd957cc1ad94e077efe
Ian Chou <iantpe@hotmail.com>
2025-05-15 02:41:54 +0800

    feat: add author profile images and data structure

---
+++ app/data/authors.ts
@@ -0,0 +1,13 @@
+export const authors = {
+  ianchou: {
+    name: 'Ian Chou',
+    avatar: '/images/authors/ian.png',
+    bio: 'NC/LC工程師，AI愛好者',
+  },
+  arkiiz: {
+    name: 'Arkiiz',
+    chineseName: '庭宇',
+    avatar: '/images/authors/arkiiz.png',
+    bio: 'AI講師、超級顧問',
+  },
+};
