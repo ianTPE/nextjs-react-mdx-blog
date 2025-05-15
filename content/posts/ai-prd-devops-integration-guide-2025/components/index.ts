315e92dca217980ba6c46b184ec906a070c1242c
Ian Chou <iantpe@hotmail.com>
2025-05-15 14:34:54 +0800

    feat: add AI+PRD+DevOps integration guide with interactive charts and visualizations

---
+++ content/posts/ai-prd-devops-integration-guide-2025/components/index.ts
@@ -0,0 +1,9 @@
+// Export all chart components
+export { default as CoreArchitecture } from './CoreArchitecture';
+export { default as TechStackChart } from './TechStackChart';
+export { default as IndustryDistributionPieChart } from './IndustryDistributionPieChart';
+export { default as IndustryMaturityMatrix } from './IndustryMaturityMatrix';
+export { default as KeyIndicatorCards } from './KeyIndicatorCards';
+export { default as BenefitRadarChart } from './BenefitRadarChart';
+export { default as ROITrendChart } from './ROITrendChart';
+export { default as ImplementationRoadmap } from './ImplementationRoadmap';
