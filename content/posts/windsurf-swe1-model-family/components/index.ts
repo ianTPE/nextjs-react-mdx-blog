f8af9a137c4ca892d4e843530f96c00e36be92b4
Ian Chou <iantpe@hotmail.com>
2025-05-16 06:18:28 +0800

    feat: add MDX component system with custom table and code block components

---
+++ content/posts/windsurf-swe1-model-family/components/index.ts
@@ -0,0 +1,13 @@
+// Export basic table components
+export { Table, THead, TBody, Th, Td, Tr } from './Table';
+
+// Export specialized table components
+export { default as ModelComparisonTable } from './ModelComparisonTable';
+export { default as TechnicalSpecsTable } from './TechnicalSpecsTable';
+export { default as PerformanceBenchmarkTable } from './PerformanceBenchmarkTable';
+
+// Export CodeBlock component
+export { default as CodeBlock } from './CodeBlock';
+
+// Export MDX components
+export { default as mdxComponents } from './MDXComponents';
