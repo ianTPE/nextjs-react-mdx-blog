f5fe402ea3289a523c2b6810b677bfec7fac8425
Ian Chou <iantpe@hotmail.com>
2025-05-16 20:18:01 +0800

    feat: add CustomButton component with alert interaction and styling

--- content/posts/mdx-blog-setup/components/CustomButton.tsx
+++ content/posts/mdx-blog-setup/components/CustomButton.tsx
@@ -18,21 +18,22 @@ const CustomButton = ({
   }, [message]);
 
   return (
-    <button
-      type="button"
-      onClick={handleClick}
-      className={`
-        px-4 py-2 
-        bg-blue-600 text-white 
-        rounded-lg 
-        hover:bg-blue-700 
-        transition-colors duration-200
-        focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2
-        ${className}
-      `}
-    >
-      {children}
-    </button>
+<button
+  type="button"
+  onClick={handleClick}
+  className={`
+    px-4 py-2
+    bg-white text-blue-600 font-semibold
+    border-2 border-blue-600
+    rounded-full
+    hover:bg-blue-50
+    focus:outline-none focus:ring-2 focus:ring-blue-300
+    transition-all duration-200
+    ${className}
+  `}
+>
+  {children}
+</button>
   );
 };
 
