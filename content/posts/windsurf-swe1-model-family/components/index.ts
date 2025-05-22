576bce05e16c1587ba0bc22c77070e897c6b2776
Ian Chou <iantpe@hotmail.com>
2025-05-22 09:01:38 +0800

    feat: add benchmark chart component and blog post for Windsurf SWE-1 model family

--- content/posts/windsurf-swe1-model-family/components/index.ts
+++ content/posts/windsurf-swe1-model-family/components/index.ts
@@ -1,20 +1,7 @@
-/**
- * Custom components for the windsurf-swe1-model-family blog post
- */
+"use client";
 
-// Export basic table components
-export { Table, THead, TBody, Th, Td, Tr } from './Table';
+import EndToEndBenchmarkChart from './EndToEndBenchmarkChart';
 
-// Export specialized table components
-export { default as ModelComparisonTable } from './ModelComparisonTable';
-export { default as TechnicalSpecsTable } from './TechnicalSpecsTable';
-export { default as PerformanceBenchmarkTable } from './PerformanceBenchmarkTable';
-export { default as HumanEvalTable } from './HumanEvalTable';
-export { default as SWEBenchTable } from './SWEBenchTable';
-export { default as MultiFileContextTable } from './MultiFileContextTable';
-export { default as LatencyTable } from './LatencyTable';
-export { default as ResourceConsumptionTable } from './ResourceConsumptionTable';
-export { default as DevelopmentAccelerationTable } from './DevelopmentAccelerationTable';
-
-// Export CodeBlock component
-export { default as CodeBlock } from './CodeBlock';
+export {
+  EndToEndBenchmarkChart
+};
