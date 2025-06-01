833c657c5b75c25194c5b0b9c9ffcd6cc1a47151
Ian Chou <iantpe@hotmail.com>
2025-06-02 00:51:15 +0800

    feat: add interactive table components with predefined table types for PRD guide

--- content/posts/Low-Code-No-Code-PRD-Complete-Guide-Part2/components/index.ts
+++ content/posts/Low-Code-No-Code-PRD-Complete-Guide-Part2/components/index.ts
@@ -1,5 +1,35 @@
-import MarkdownDisplay from './MarkdownDisplay';
-import MermaidDiagram from './MermaidDiagram';
-import InteractiveTable from './InteractiveTable';
+"use client";
 
-export { MarkdownDisplay, MermaidDiagram, InteractiveTable };
+// 導出所有表格組件
+export {
+  default as InteractiveTable,
+  VersionHistoryTable,
+  TermTable,
+  KPITable,
+  PlatformComparisonTable,
+  RolePermissionTable,
+  DataFieldTable,
+  InteractionEventTable,
+  BusinessRuleTable,
+  IntegrationMappingTable,
+  TestCaseTable,
+  ProjectModuleTable,
+  RiskTable,
+} from './InteractiveTable';
+
+// 導出所有類型
+export type {
+  InteractiveTableProps,
+  VersionHistoryData,
+  TermData,
+  KPIData,
+  PlatformComparisonData,
+  RolePermissionData,
+  DataFieldData,
+  InteractionEventData,
+  BusinessRuleData,
+  IntegrationMappingData,
+  TestCaseData,
+  ProjectModuleData,
+  RiskData,
+} from './InteractiveTable';
