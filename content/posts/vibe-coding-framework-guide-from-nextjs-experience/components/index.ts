cd5d80c38d08b5554a5488adb3101832a6156942
Ian Chou <iantpe@hotmail.com>
2025-06-03 02:34:05 +0800

    feat: add interactive React Flow decision tree component for framework selection guide

--- content/posts/vibe-coding-framework-guide-from-nextjs-experience/components/index.ts
+++ content/posts/vibe-coding-framework-guide-from-nextjs-experience/components/index.ts
@@ -3,3 +3,4 @@
 export { default as FrameworkComparisonTable } from './FrameworkComparisonTable';
 export { default as VibeCodingDecisionTree } from './VibeCodingDecisionTree';
 export { default as CSSDecisionTree } from './CSSDecisionTree';
+export { default as ReactFlowDecisionTree } from './ReactFlowDecisionTree';
