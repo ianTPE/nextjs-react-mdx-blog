b1c21b3a85da3d75edbe55ea7fafe7aca6e8ad24
Ian Chou <iantpe@hotmail.com>
2025-06-04 03:44:00 +0800

    feat: add MarkdownDisplay component with styled markdown rendering

--- content/posts/mdx-blog-frontend-backend-separation-nextjs-to-hono-guide/components/index.ts
+++ content/posts/mdx-blog-frontend-backend-separation-nextjs-to-hono-guide/components/index.ts
@@ -5,11 +5,13 @@ import Stage2Architecture from './Stage2Architecture';
 import Stage3Architecture from './Stage3Architecture';
 import EvolutionOverview from './EvolutionOverview';
 import PerformanceComparison from './PerformanceComparison';
+import MarkdownDisplay from './MarkdownDisplay';
 
 export {
   Stage1Architecture,
   Stage2Architecture,
   Stage3Architecture,
   EvolutionOverview,
-  PerformanceComparison
+  PerformanceComparison,
+  MarkdownDisplay
 };
