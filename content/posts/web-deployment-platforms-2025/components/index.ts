94ae1a6d231a61bf7067b5312acf5a225f763048
Ian Chou <iantpe@hotmail.com>
2025-06-03 11:41:29 +0800

    feat: add performance comparison charts for web deployment platforms

---
+++ content/posts/web-deployment-platforms-2025/components/index.ts
@@ -0,0 +1,15 @@
+"use client";
+
+import PerformanceComparisonChart from './PerformanceComparisonChart';
+import CostComparisonChart from './CostComparisonChart';
+import GlobalLatencyChart from './GlobalLatencyChart';
+import ConcurrencyChart from './ConcurrencyChart';
+import FrameworkSupportChart from './FrameworkSupportChart';
+
+export {
+  PerformanceComparisonChart,
+  CostComparisonChart,
+  GlobalLatencyChart,
+  ConcurrencyChart,
+  FrameworkSupportChart
+};
