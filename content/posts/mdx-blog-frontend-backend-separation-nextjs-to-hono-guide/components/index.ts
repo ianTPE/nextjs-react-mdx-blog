da25b7efa258f7a8bdc8cb6021c46ae9d506a180
Ian Chou <iantpe@hotmail.com>
2025-06-04 05:43:46 +0800

    feat: add new blog post about migrating from NextJS to Hono with component exports

--- content/posts/mdx-blog-frontend-backend-separation-nextjs-to-hono-guide/components/index.ts
+++ content/posts/mdx-blog-frontend-backend-separation-nextjs-to-hono-guide/components/index.ts
@@ -5,15 +5,11 @@ import Stage2Architecture from './Stage2Architecture';
 import Stage3Architecture from './Stage3Architecture';
 import EvolutionOverview from './EvolutionOverview';
 import PerformanceComparison from './PerformanceComparison';
-import MarkdownDisplay from './MarkdownDisplay';
-import AsciiDiagram from './AsciiDiagram';
 
 export {
   Stage1Architecture,
   Stage2Architecture,
   Stage3Architecture,
   EvolutionOverview,
-  PerformanceComparison,
-  MarkdownDisplay,
-  AsciiDiagram
+  PerformanceComparison
 };
