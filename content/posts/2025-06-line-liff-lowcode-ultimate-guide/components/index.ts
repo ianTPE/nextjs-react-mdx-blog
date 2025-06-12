af5d7053800cf0069a81069d0dcf463ccb612adc
Ian Chou <iantpe@hotmail.com>
2025-06-12 11:20:58 +0800

    feat: add MDX blog components for low-code platform comparison tables

---
+++ content/posts/2025-06-line-liff-lowcode-ultimate-guide/components/index.ts
@@ -0,0 +1,7 @@
+export * from "./PlatformCategoryTable";
+export * from "./PlatformRecommendationTable";
+export * from "./PricingPlanTable";
+export * from "./CostComparisonTable";
+export * from "./IndustryComplianceTable";
+export * from "./MarkdownDisplay";
+export * from "./MermaidDiagram";
