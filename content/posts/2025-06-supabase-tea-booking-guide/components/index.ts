2eddad693d24f49de001df6e3543399684a6c9d2
Ian Chou <iantpe@hotmail.com>
2025-06-15 15:22:41 +0800

    feat: add React components for Supabase guide article

---
+++ content/posts/2025-06-supabase-tea-booking-guide/components/index.ts
@@ -0,0 +1,13 @@
+"use client";
+
+import ThreeWayComparisonTable from './ThreeWayComparisonTable';
+import SupabaseSetupSteps from './SupabaseSetupSteps';
+import SupabasePricingTable from './SupabasePricingTable';
+import FinalComparisonMatrix from './FinalComparisonMatrix';
+
+export {
+  ThreeWayComparisonTable,
+  SupabaseSetupSteps,
+  SupabasePricingTable,
+  FinalComparisonMatrix
+};
