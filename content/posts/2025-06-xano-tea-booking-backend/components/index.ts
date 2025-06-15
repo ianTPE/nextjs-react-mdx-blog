b4347e9085c7b3d2a843ee79fc908f43bb9d0538
Ian Chou <iantpe@hotmail.com>
2025-06-15 11:34:33 +0800

    feat: add Xano API documentation components and endpoints for tea booking system

--- content/posts/2025-06-xano-tea-booking-backend/components/index.ts
+++ content/posts/2025-06-xano-tea-booking-backend/components/index.ts
@@ -2,5 +2,22 @@
 
 import BubbleXanoComparisonTable from './BubbleXanoComparisonTable';
 import BackendComparisonTable from './BackendComparisonTable';
+import XanoPricingTable from './XanoPricingTable';
+import XanoSetupSteps from './XanoSetupSteps';
+import APIEndpoint from './APIEndpoint';
+import GetUserOrdersEndpoint from './GetUserOrdersEndpoint';
+import QueuePredictionEndpoint from './QueuePredictionEndpoint';
+import LineUserAuthEndpoint from './LineUserAuthEndpoint';
+import SimpleAPIEndpoint from './SimpleAPIEndpoint';
 
-export { BubbleXanoComparisonTable, BackendComparisonTable };
+export { 
+  BubbleXanoComparisonTable, 
+  BackendComparisonTable, 
+  XanoPricingTable, 
+  XanoSetupSteps, 
+  APIEndpoint,
+  GetUserOrdersEndpoint,
+  QueuePredictionEndpoint,
+  LineUserAuthEndpoint,
+  SimpleAPIEndpoint
+};
