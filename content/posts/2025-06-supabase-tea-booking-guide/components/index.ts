46395a8cb7f808facb8b436cac78db977854b23e
Ian Chou <iantpe@hotmail.com>
2025-06-15 15:32:36 +0800

    feat: add cost comparison component and export index for Supabase guide

--- content/posts/2025-06-supabase-tea-booking-guide/components/index.ts
+++ content/posts/2025-06-supabase-tea-booking-guide/components/index.ts
@@ -4,10 +4,12 @@ import ThreeWayComparisonTable from './ThreeWayComparisonTable';
 import SupabaseSetupSteps from './SupabaseSetupSteps';
 import SupabasePricingTable from './SupabasePricingTable';
 import FinalComparisonMatrix from './FinalComparisonMatrix';
+import ThreePlatformCostComparison from './ThreePlatformCostComparison';
 
 export {
   ThreeWayComparisonTable,
   SupabaseSetupSteps,
   SupabasePricingTable,
-  FinalComparisonMatrix
+  FinalComparisonMatrix,
+  ThreePlatformCostComparison
 };
